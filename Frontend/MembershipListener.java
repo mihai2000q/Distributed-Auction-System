7a490441363eb1bbed75d361ea66a05cfa24f80c
Mihai <FilipCristiMihail@yahoo.com>
2022-12-08 22:19:03 +0100

    new class - Membership Listener

---
+++ Frontend/MembershipListener.java
@@ -0,0 +1,24 @@
+import org.jgroups.Address;
+import org.jgroups.View;
+
+public class MembershipListener implements org.jgroups.MembershipListener {
+    @Override
+    public void viewAccepted(View view) {
+
+    }
+
+    @Override
+    public void suspect(Address address) {
+
+    }
+
+    @Override
+    public void block() {
+
+    }
+
+    @Override
+    public void unblock() {
+
+    }
+}
