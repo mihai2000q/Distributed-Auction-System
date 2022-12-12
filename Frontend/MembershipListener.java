3e2e312894a7786099f25bd188cd8996ffb9329c
Mihai <FilipCristiMihail@yahoo.com>
2022-12-12 21:55:43 +0100

    Update MembershipListener

--- Frontend/MembershipListener.java
+++ Frontend/MembershipListener.java
@@ -4,21 +4,21 @@ import org.jgroups.View;
 public class MembershipListener implements org.jgroups.MembershipListener {
     @Override
     public void viewAccepted(View view) {
-
+        System.out.println("\nJgroups view changed\nnew view: " + view.toString() + "\n");
     }
 
     @Override
     public void suspect(Address address) {
-
+        System.out.println("\nJgroups view suspected member crash: " + address.toString() + "\n");
     }
 
     @Override
     public void block() {
-
+        System.out.println("\nJgroups view block indicator\n");
     }
 
     @Override
     public void unblock() {
-
+        System.out.println("\nJgroups view unblock indicator\n");
     }
 }
